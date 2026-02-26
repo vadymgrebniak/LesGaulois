aa41a92fbaba6409ec69fddda57592921beeabbc
Vadym Grebniak <grv5756a@ensfsi.onmicrosoft.com>
2026-02-26 14:58:40 +0100

    added two characters

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+}
