ab5f2684c246960d5e64edc9063085f8820d41e8
Vadym Grebniak <grv5756a@u3-212-06.univ-tlse3.fr>
2026-03-17 19:58:37 +0100

    AAAAAAASSSSSSSEEEEEEERRRRRRRRTTTTTTTTTTTgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit statusgit status

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,17 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", -6);
+		System.out.println(minus.getNom());
+		
+	}
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() == true;		
 	}
 
 	public String getNom() {
@@ -29,6 +36,16 @@ public class Romain {
 		else {
 			parler("J'abandonne !");
 			force = 0;
+		}
+	}
+	
+	private Boolean isInvariantVerified() {
+		if (force >= 0) {
+			
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	
