931b44416fe4d171b546899a766db9441dd6e498
Skamer-li <plazerovich@gmail.com>
2026-03-31 11:35:21 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,12 +1,19 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	int nbEquipement = 0;
 
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 12);
-		System.out.println(minus.getNom());
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 
 	public Romain(String nom, int force) {
@@ -23,10 +30,6 @@ public class Romain {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
 
-	private String prendreParole() {
-		return "Le Romain " + nom + " : ";
-	}
-
 	public void recevoirCoup(int forceCoup) {
 		assert forceCoup > 0;
 
@@ -36,13 +39,52 @@ public class Romain {
 
 		if (this.force > 0) {
 			parler("Aïe");
-		} 
-		else {
+		} else {
 			parler("J'abandonne !");
 			this.force = 0;
 		}
-		
+
 		assert this.force < forceRomainDebut;
+	}
+
+	public void sEquiper(Equipement equipement) {
+		Boolean isEquiped = false;
+
+		switch (nbEquipement) {
+		case 0:
+			equipements[0] = equipement;
+			isEquiped = true;
+			break;
+
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println(adresserSoldat() + " possède déjà un " + equipement.toString() + ".");
+			} else {
+				equipements[1] = equipement;
+				isEquiped = true;
+			}
+			break;
+
+		case 2:
+			System.out.println(adresserSoldat() + " est déjà bien protégé !");
+			break;
+
+		default:
+			System.out.println("nbEquipement n'est pas valdie !");
+		}
+
+		if (isEquiped) {
+			System.out.println(adresserSoldat() + " s’équipe avec un " + equipement.toString() + ".");
+			nbEquipement++;
+		}
+	}
+
+	private String prendreParole() {
+		return "Le Romain " + nom + " : ";
+	}
+
+	private String adresserSoldat() {
+		return "Le soldat " + this.nom;
 	}
 
 	private Boolean isInvariantVerified() {
